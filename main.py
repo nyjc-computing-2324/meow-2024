c047a0257ed166125ba7f6738d9708fafc1fa8e1
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-03-08 22:48:45 +0800

    Revert "Added Routes for MVP (#10)" (#11)
    This reverts commit 78439ec5513060f296a7333a8a00cd44c5bd1b4d.

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route('/login', methods = ["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template("login.html")
-    else:
-        #get username and password
-        #compare to database
-        #log user in
-        return redirect("/")
-
-@app.route('/register', methods = ["GET", "POST"])
-def index():
-    if request.method == "GET":
-        return render_template("register.html")
-    else:
-        #validate username and password
-        #add to database
-        #log user in
-        return redirect("/")
+    return 'Hello from Flask!'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
