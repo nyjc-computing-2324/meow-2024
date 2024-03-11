0eb0c4cd0e4e3fbde9dbd2f8a73e9395b434b89a
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-03-11 07:01:09 +0000

    pull from dev

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return render_template("index.html")
+
+@app.route('/login', methods = ["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        #get username and password
+        #compare to database
+        #log user in
+        return redirect("/")
+
+@app.route('/register', methods = ["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        #validate username and password
+        #add to database
+        #log user in
+        return redirect("/")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
