4645a6bbf301aecdf019242ad3050884a1c90482
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-03-14 19:15:56 +0000

    moved implementation of route handlers to view.py

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask
+import view
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return view.index()
 
 @app.route('/home')
 def home():
-    return render_template("temp.html")
+    return view.temp()
 
 @app.route('/about')
 def about():
-    return render_template("temp.html")
+    return view.temp()
 
 @app.route('/login', methods = ["GET", "POST"])
 def login():
-    return render_template("index.html")
-    if request.method == "GET":
-        return render_template("login.html")
-    else:
-        #get username and password
-        #compare to database
-        #log user in
-        return redirect("/")
+    return view.login()
 
 @app.route('/register', methods = ["GET", "POST"])
 def register():
-    if request.method == "GET":
-        return render_template("register.html")
-    else:
-        #validate username and password
-        #add to database
-        #log user in
-        return redirect("/")
+    return view.register()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
