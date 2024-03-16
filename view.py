3e08ee182c19a5f49379011e261edb7ca8a73df0
mc4706 <141309318+mc4706@users.noreply.github.com>
2024-03-16 08:11:10 +0000

    put in tran's lauch page code, wrote app routes, redirect the users to temp when they go to any page

--- view.py
+++ view.py
@@ -1,34 +1,52 @@
 from flask import Flask, render_template, redirect, request, session
 
+# completed is a bool, by default False.
+completed = False
+
+@app.route("/")
 def index():
-    #display index.html
+    #display index.html which is the launch page
+    if not completed:
+        # redirects the user to temp
+        return redirect("/temp")
     return render_template("index.html")
 
+@app.route("/temp")
 def temp():
-    #display temp.html
     return render_template("temp.html")
+
 
 def home():
     #display home.html
     #yeah it doesn't exist yet just ignore this first
     raise NotImplementedError
 
+
 def about():
     #display about.html
     #also doesn't exist yet
     raise NotImplementedError
 
+@app.route("/login", methods=["GET", "POST"])
 def login():
     #display login.html
-    if request.method == "GET":
-        return render_template("login.html")
+    if completed:
+        if request.method == "GET":
+            return render_template("login.html")
+        # for here, the validation needs to be implemented
+        else:
+            return redirect("/home")
     else:
-        return redirect("/home")
+        return redirect("/temp")
 
+@app.route("/register", methods=["GET", "POST"])
 def register():
     #display register.html
-    if request.method == "GET":
-        return render_template("register.html")
+    if completed:
+        if request.method == "GET":
+            return render_template("register.html")
+        # for here the validation needs to be implemented
+        else:
+            return redirect("/home")
     else:
-        return redirect("/home")
-    
+        return redirect("/temp")
