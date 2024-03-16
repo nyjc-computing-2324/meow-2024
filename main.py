73219ae80f1a24dfb346784b48144e5c599bf849
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-03-16 15:03:55 +0800

    Dev (#24)
    * Added Routes for MVP
    
    Added routes for:
    - Homepage (/)
    - Login (/login)
    - Register (/register)
    
    * Organised Repo
    
    * Added the skeleton code to enable storage of accounts
    
    * added skeleton code for validation functions to implement login and register
    
    * quick update to argument of password_isvalid()
    
    * Backend routing to Backend (#18)
    
    * pull from dev
    
    * New routes
    
    Added routes for:
    - Home page
    - About page
    
    * Backend database (#19)
    
    * added skeleton code for validation functions to implement login and register
    
    * quick update to argument of password_isvalid()
    
    * Added Routes for MVP
    
    Added routes for:
    - Homepage (/)
    - Login (/login)
    - Register (/register)
    
    * Revert "Added Routes for MVP (#10)" (#11)
    
    This reverts commit 78439ec5513060f296a7333a8a00cd44c5bd1b4d.
    
    * Organised Repo
    
    ---------
    
    Co-authored-by: Ling Kai <94221160+ratSpoT@users.noreply.github.com>
    
    * Frontend (#22)
    
    * I did the overall launch, register and login page. The Frontend.py is used to run the code.
    
    I need functions validate_login(username, password) which needs to be implemented by backend. It should check if username exists, then check if password is correct. This function should return a False if either or both requirements are not fulfilled. Else it should return true. I also need a validate_register(username, password) which returns False if username and password does not match the requirements and returns True if it fulfils the requirements
    
    * Created nav bars
    
    * Did temp. Did the base code for nav, index and login.
    
    * Added the skeleton code to enable storage of accounts
    
    * added skeleton code for validation functions to implement login and register
    
    * quick update to argument of password_isvalid()
    
    * Added Routes for MVP
    
    Added routes for:
    - Homepage (/)
    - Login (/login)
    - Register (/register)
    
    * Revert "Added Routes for MVP (#10)" (#11)
    
    This reverts commit 78439ec5513060f296a7333a8a00cd44c5bd1b4d.
    
    * Organised Repo
    
    * Backend routing to Backend (#18)
    
    * pull from dev
    
    * New routes
    
    Added routes for:
    - Home page
    - About page
    
    * Backend database (#19)
    
    * added skeleton code for validation functions to implement login and register
    
    * quick update to argument of password_isvalid()
    
    * Added Routes for MVP
    
    Added routes for:
    - Homepage (/)
    - Login (/login)
    - Register (/register)
    
    * Revert "Added Routes for MVP (#10)" (#11)
    
    This reverts commit 78439ec5513060f296a7333a8a00cd44c5bd1b4d.
    
    * Organised Repo
    
    ---------
    
    Co-authored-by: Ling Kai <94221160+ratSpoT@users.noreply.github.com>
    
    * Fixed routes
    
    every app route was set as def index() mb
    
    * re-added imports and fixed landing page
    
    * moved implementation of route handlers to view.py
    
    ---------
    
    Co-authored-by: mc4706 <141309318+mc4706@users.noreply.github.com>
    Co-authored-by: Ling Kai <94221160+ratSpoT@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kantanana <85265596+kantanana@users.noreply.github.com>
    Co-authored-by: Ling Kai <94221160+ratSpoT@users.noreply.github.com>
    Co-authored-by: mc4706 <141309318+mc4706@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import view
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return view.index()
+
+@app.route('/home')
+def home():
+    return view.temp()
+
+@app.route('/about')
+def about():
+    return view.temp()
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    return view.login()
+
+@app.route('/register', methods = ["GET", "POST"])
+def register():
+    return view.register()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
