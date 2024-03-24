6d331d2885d49966ab20f0dcc1604c4e7cbc1060
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-03-24 21:18:21 +0800

    Revert "Frontend (#54)"
    This reverts commit 312ae9dc7e99f04ca1cedac5df112fd4439b80d1.

--- view.py
+++ view.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template, redirect, request, session
 
-# completed is a bool, by default False.
-completed = False
-
 def index():
-    #display index.html which is the launch page
-    if not completed:
-        # redirects the user to temp
-        return redirect("/temp")
+    #display index.html
     return render_template("index.html")
 
 def temp():
+    #display temp.html
     return render_template("temp.html")
-  
+
 def home():
     #display home.html
     #yeah it doesn't exist yet just ignore this first
@@ -25,22 +20,15 @@ def about():
 
 def login():
     #display login.html
-    if completed:
-        if request.method == "GET":
-            return render_template("login.html")
-        # for here, the validation needs to be implemented
-        else:
-            return redirect("/home")
+    if request.method == "GET":
+        return render_template("login.html")
     else:
-        return redirect("/temp")
+        return redirect("/home")
 
 def register():
     #display register.html
-    if completed:
-        if request.method == "GET":
-            return render_template("register.html")
-        # for here the validation needs to be implemented
-        else:
-            return redirect("/home")
+    if request.method == "GET":
+        return render_template("register.html")
     else:
-        return redirect("/temp")
+        return redirect("/home")
+    
