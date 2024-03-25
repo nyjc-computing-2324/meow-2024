461576840cc6f63737c77789facb68c5bc619b3b
saathvik28 <125167997+saathvik28@users.noreply.github.com>
2024-03-25 02:46:47 +0000

    unittest again

--- tests.py
+++ tests.py
@@ -5,8 +5,11 @@ from database import Account
 We will be using the unittest module instead to test frontend and backend.
 Using this module follows a similar OOP structure to the classes we made last session 
 One key difference is that you need to inherit from the 'unittest.TestCase' baseclass
+Methods must also start with 'test' as below, else unittest can't recognise and run them
 
-DataQuest Simple Guide:https://www.dataquest.io/blog/unit-tests-python/
+Read these guides to familiarise yourselves:
+
+DataQuest Simple Guide: https://www.dataquest.io/blog/unit-tests-python/
 
 unittest Documentation: https://docs.python.org/3/library/unittest.html
 """
