1c3f494854bec11860a8c796c7fc360c52b0cbbf
saathvik28 <125167997+saathvik28@users.noreply.github.com>
2024-04-09 14:24:27 +0000

    Used authentication for testing Account,  Improved some tests

--- tests/Test_Backend.py
+++ tests/Test_Backend.py
@@ -1,5 +1,6 @@
 from unittest import *
 from database import Account
+import auth
 import sqlite3
 #IMPORTANT Please Read
 """
@@ -28,14 +29,30 @@ class Test_Account(TestCase):
         self.Account = Account(':memory:')
         self.name = '@aBc'
         self.password = 'Abcd1234'
-        self.Account.insert(self.name,self.password)
+        self.password_hash, self.salt = auth.create_hash(self.password)
+        self.Account.insert(self.name, self.password_hash, self.salt)
 
     def test_insert(self):
         """
         Checks if the provided data is correctly inserted into
-        the Account table with the insert method
+        the Account table with the insert method, ie. if the name
+        attribute of the retrieved record matches with the
+        inserted record's name
         """
-        self.assertTrue(self.Account.retrieve('username',self.name),'Insert function failed')
+        with sqlite3.connect(self.Account.database_name) as conn:
+            cursor = conn.cursor()
+            query = """
+                    SELECT *
+                    FROM "Account"
+                    WHERE "username" == ?;
+                    """
+            params = (self.name,)
+            cursor.execute(query, params)
+            record = cursor.fetchone()
+            conn.commit()
+        self.assertIsNotNone(record, 'Record was not inserted at all')
+        self.assertEqual(record[2], self.password_hash, 'Password inserted wrongly')
+        self.assertEqual(record[3], self.salt, 'Salt inserted wrongly')
         self.result_insert = TextTestRunner().run(defaultTestLoader.loadTestsFromName("test_insert"))
 
     def test_update(self):
@@ -47,8 +64,9 @@ class Test_Account(TestCase):
         if not self.result_insert.wasSuccessful():
             self.skipTest("Skipping test condition as insertion does not work")
             
-        self.password = "aBCD1234"
-        self.Account.update(1, 'password', self.password)
+        self.new_password = "aBCD1234"
+        self.new_password_hash, _ = auth.create_hash(self.new_password)
+        self.Account.update(1, 'password', self.new_password_hash)
         with sqlite3.connect(self.Account.database_name) as conn:
             cursor = conn.cursor()
             query = """
@@ -56,11 +74,12 @@ class Test_Account(TestCase):
                     FROM "Account"
                     WHERE "password" == ?;
                     """
-            params = (self.password,)
+            params = (self.new_password_hash,)
             cursor.execute(query, params)
             record = cursor.fetchone()
             conn.commit()
-        self.assertEqual(record[2], self.password, "Update method failed")
+        self.assertIsNotNone(record, 'Record somehow got deleted during updating')
+        self.assertEqual(record[2], self.new_password_hash, "Update method failed")
 
     def test_retrieve(self):
         """
@@ -71,8 +90,8 @@ class Test_Account(TestCase):
         if not self.result_insert.wasSuccessful():
             self.skipTest("Skipping test condition as insertion does not work")
             
-        self.assertEqual(self.name, self.Account.retrieve('username',self.name)[1], 'Retrieve method failed using username')      
-        self.assertEqual(self.name, self.Account.retrieve('account_id','1')[1], 'Retrieve method failed using account_id')
+        self.assertEqual(self.password_hash, self.Account.retrieve('username',self.name)[2], 'Retrieve method failed using username')      
+        self.assertEqual(self.password_hash, self.Account.retrieve('account_id','1')[2], 'Retrieve method failed using account_id')
         
 
     def test_delete(self):
@@ -89,7 +108,8 @@ class Test_Account(TestCase):
         self.Account.delete('username',del_target[0])
         self.assertIsNone(self.Account.retrieve('username', self.name), 'Delete method failed using username')
         
-        self.Account.insert(self.name,self.password)
+        password, salt = auth.create_hash(self.password)
+        self.Account.insert(self.name,password,salt)
         self.Account.delete('account_id',del_target[0])
         self.assertIsNone(self.Account.retrieve('username', self.name), 'Delete method failed using account_id')
         
