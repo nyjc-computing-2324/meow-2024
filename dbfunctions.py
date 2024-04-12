0bb629bf06e19022d4e67fee75f14c134bf5e28f
xinyu2309 <125541832+xinyu2309@users.noreply.github.com>
2024-04-12 05:46:50 +0000

    retrieve_account and delete_account

--- dbfunctions.py
+++ dbfunctions.py
@@ -13,6 +13,33 @@ def create_account(username: str, password: str):
         student_account.insert(username, password, salt)
         student_account_backup.insert(username, password, salt)
 
+def retrieve_account(field: str, data: str) -> tuple:
+    """
+    xinyu
+    check if account exists
+    if false raise attribute error
+    if true return a dictionary of account_id, username, password, salt
+    """
+    record = student_account.retrieve(field, data)
+    if record is None:
+        raise AttributeError("Account does not exist.")
+    account_id, username, password, salt = record
+    record_dict = {'account_id': account_id, 'username': username, 'password': password, 'salt': salt}
+    return record_dict
+    
+
+def delete_account(field: str, data: str):
+    """
+    xinyu
+    check if account exists
+    if false raise attribute error
+    if true delete account from student_account and student_account_backup
+    """
+    if student_account.retrieve("username", username) is None:
+        raise AttributeError("Account does not exist.")
+    student_account.delete(field, data)
+    student_account_backup.delete(field, data)
+
 def login(username: str , password: str) -> bool:
     # checks for valid username and password is already done 
 
@@ -24,3 +51,4 @@ def login(username: str , password: str) -> bool:
     account_id, database_username, database_password, database_salt = data
     # salting and hashing of password implemented 
     return auth.check_password(password, database_password, database_salt)
+
