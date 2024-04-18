dd0cca1d6836b5262588fdcb12e96da3575ea13f
Rae Tan <124945015+raetan2023@users.noreply.github.com>
2024-04-18 12:04:22 +0800

    Update backend-database-to-backend.sh

--- scripts/backend-database-to-backend.sh
+++ scripts/backend-database-to-backend.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/nix/store/3x9jq3l68vxy0ap48xy500g6czvzavhm-bash/bin/bash
 
 # Import git identity check
@@ -17,3 +18,24 @@ git push
 chmod 700 ./scripts/backend-database-to-backend.sh
 
 # Only devops can run
+=======
+#!/nix/store/3x9jq3l68vxy0ap48xy500g6czvzavhm-bash/bin/bash
+
+# Import git identity check
+sh ./scripts/git-identity.sh
+
+# Merge backend-database into backend
+git switch backend-database
+git pull
+git switch backend
+git pull
+git merge backend-database
+echo "Merged backend-database into backend"
+git commit -m "Merged backend-database into backend"
+git push
+
+# Update permissions to +x
+chmod 700 ./scripts/backend-database-to-backend.sh
+
+# Only devops can run
+>>>>>>> d1ffd84 (Update backend-database-to-backend.sh)
