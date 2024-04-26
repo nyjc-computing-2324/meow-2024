4a2dad7ded68079d743875338d41ce04b00905c2
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-04-26 13:48:00 +0800

    Revert "Fix backend database Table functionality (#208)"
    This reverts commit 0d8a1798a43756592195a7c647d38997b471b19d.

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from flask import Flask, redirect, request, session
-import view, validate, database, dbfunctions
-import os
-
+import view, validate, database
 
 app = Flask(__name__)
 
-app.secret_key = os.urandom(32)
-
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        session["logged_in"] = False
     return view.index()
 
 @app.route('/temp')
@@ -25,14 +19,6 @@ def home():
 def about():
     return view.about()
 
-@app.route('/pp')
-def pp():
-    return view.pp()
-
-@app.route('/tac')
-def tac():
-    return view.tac()
-
 @app.route('/login', methods = ["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -40,9 +26,8 @@ def login():
     else:
         username = request.form["username"]
         password = request.form["password"]
-        if dbfunctions.login(username, password):
+        if validate.user_isvalid(username, password):
             session["logged_in"] = True
-            session["user"] = username
             return redirect("/home")
         else:
             return view.login(error="invalid username or password")
@@ -56,29 +41,17 @@ def register():
         password = request.form["password"]
         if validate.username_isvalid(username):
             if validate.password_isvalid(password):
-                dbfunctions.create_account(username, password)
+                database.create_account(username, password)
                 session["logged_in"] = True
-                session["user"] = username
                 return redirect("/home")
             else:
                 return view.register(error="Password does not meet requirements.")
         else:
             return view.register(error="Username does not meet requirements.")
 
-@app.route('/profile', methods = ["GET", "POST"])
+@app.route('/profile')
 def profile():
-    if request.method == "POST":
-        if request.form["response"] == "Edit":
-            return view.profile(edit = True)
-        elif request.form["response"] == "Save":
-            return view.profile(edit = False)
-        elif request.form["response"] == "Cancel":
-            return view.profile(edit = False)
     return view.profile()
-
-@app.route('/profile_edit', methods = ["GET", "POST"])
-def profile_edit():
-    return view.profile_edit()
 
 @app.route('/view_edit_cca')
 def view_edit_cca():
@@ -91,10 +64,6 @@ def records_cca():
 @app.route('/records_activities')
 def records_activities():
     return view.records_activities()
-
-@app.route('/view_edit_activities')
-def view_edit_activities():
-    return view.view_edit_activities()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
--- view.py
+++ view.py
@@ -7,32 +7,25 @@ def index():
     # display index.html which is the launch page
     return render_template("index.html")
 
-def view_edit_activities():
+def view_edit_cca():
     # display view_edit_cca.html
     if not completed:
         # redirects the user to temp
         return redirect("/temp")
-    return render_template("view_edit_activities.html")
-
-def view_edit_cca():
-    # display view_edit_cca.html
-    # if not completed:
-    #     # redirects the user to temp
-    #     return redirect("/temp")
     return render_template("view_edit_cca.html")
 
 def records_cca():
     # display records_cca.html
-    # if not completed:
-    #     # redirects the user to temp
-    #     return redirect("/temp")
+    if not completed:
+        # redirects the user to temp
+        return redirect("/temp")
     return render_template("records_cca.html")
 
 def records_activities():
     # display records_activities.html
-    # if not completed:
-    #     # redirects the user to temp
-    #     return redirect("/temp")
+    if not completed:
+        # redirects the user to temp
+        return redirect("/temp")
     return render_template("records_activities.html")
 
 def temp():
@@ -43,16 +36,12 @@ def home():
     #display home.html
     return render_template("home.html")
 
-def profile(edit = False, profile = None):
+def profile():
     # displays profile.html
-    # if not completed:
-    #     # redirects the user to temp.html
-    #     return redirect("/temp")
-    return render_template("profile.html", edit = edit, profile = profile)
-
-def profile_edit():
-    #displays profile_edit.html
-    return render_template("profile_edit.html")
+    if not completed:
+        # redirects the user to temp.html
+        return redirect("/temp")
+    return render_template("profile.html")
 
 def about():
     #display about.html
@@ -68,11 +57,3 @@ def login(error = ""):
 def register(error = ""):
     #display register.html
     return render_template("register.html", error_msg = error)
-
-def pp():
-    # display pravacy-policy.html
-    return render_template("privacy-policy.html")
-
-def tac():
-    # display terms-and-conditions.html
-    return render_template("terms-and-conditions.html")
