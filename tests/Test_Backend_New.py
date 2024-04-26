e0572606542811bb222d92ddbf993883eb887d15
saathvik28 <125167997+saathvik28@users.noreply.github.com>
2024-04-26 13:33:22 +0000

    New Backend Tests(Still TBC)

--- tests/Test_Backend_New.py
+++ tests/Test_Backend_New.py
@@ -3,12 +3,27 @@ from unittest import *
 
 #Instantiating table objects
 student_account = get_account('qa')
-test_student = get_student('qa')
+student_profile = get_student('qa')
 test_cca = get_cca('qa')
 test_activity = get_activity('qa')
 
 def test_create_account(username: str, password: str):
     create_account(username, password)
+    
+        
 
 def login():
     pass
+
+
+class Test_Profile(TestCase):
+    def setUp(self):
+        self.name1 = 'abcdefghi!'
+        self._class1 = '2328'
+        self.email1 = 'name@gmail.com'
+        self.name2 = 'qwertyuiop'
+        self._class1 = '2401'
+        create_profile(self.name1, self._class1, self.email1, 1)
+
+    def tearDown(self) -> None:
+    return super().tearDown()
