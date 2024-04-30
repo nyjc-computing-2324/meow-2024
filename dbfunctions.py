00ac3a624209660794d3bcd110e3a44a4ba74773
awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
2024-04-30 07:02:47 +0000

    merged backend into frontend, fixed error messages for register, and checkbox is required to register

--- dbfunctions.py
+++ dbfunctions.py
@@ -128,8 +128,8 @@ def username_taken(username: str) -> bool:
     """checks if the username is already in use, returns False if taken (temp measure)"""
     account_id = account.retrieve_primary_key(username)
     if account_id is None:
-        return True
-    return False
+        return False
+    return True
 
 def update_account(username: str, field: str, data) -> None:
     """
