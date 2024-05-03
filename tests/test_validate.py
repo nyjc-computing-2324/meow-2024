7b8244a923fb729a4854a26605f0fc12013f5aa5
saathvik28 <125167997+saathvik28@users.noreply.github.com>
2024-05-03 21:48:09 +0800

    QA(Final) (#240)
    * Fixed Create_Account test
    
    * change
    
    * wip
    
    * tests for profile functions
    
    * Test_CCA
    
    * Test_Profile(Merge Resolution)
    
    * Test_CCA and Test_ACtivity
    
    * Renamed files to lowercase, cleaned test_frontend.py
    
    * test_frontend
    
    * maybe fixes to profile tests
    
    * maybe fixes to profile tests (#235)
    
    Co-authored-by: awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>
    
    * finished tests for account functions
    
    * commit
    
    * Added docstring + comments for Test_Validate.py and made last minute changes to test username is valid method as it was outdated.
    
    * Changing
    
    * added some docstrings and comments for test account for Test_Backend_New.py
    
    * Some changes
    
    * Finally Correct(Hope I can rest)
    
    * Replaced test_backend with test_backend_new,renamed
    
    * docstrings and refinements for tests
    
    ---------
    
    Co-authored-by: Yeo Si Bin <106753997+DarkExcel@users.noreply.github.com>
    Co-authored-by: wciltea <125111866+wciltea@users.noreply.github.com>
    Co-authored-by: awpgikxdigj <68140051+awpgikxdigj@users.noreply.github.com>

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -18,55 +18,62 @@ class Test_Validate(TestCase):
 
     def setUp(self):
         """
-        Sample username/password values for testing, you guys
-        can add more values to test more cases
+        Sets up the test cases to be used in unit test methods.
         """
-        #username test cases
+        #Username test cases
+        #Valid
         self.validname = 'aBc@123_beAnsta|k'
-        self.validname_allspaces = '                            '
-
+        #Invalid
+        self.invalidname_allspaces = '                            '
         self.invalidname_notascii = 'ABCD二十八'
-        self.invalidname_notprintable = 'John \t Doe \n Smith'
+        self.invalidname_notprintable = 'John\tDoe\nSmith'
         self.invalidname_empty = ''
 
-        #password test cases
+        #Password test cases
+        #Valid
         self.validpassword = 'Abcd1234'
         self.validpassword_verylong = 'qwertty122345iuoUIOOPAHJKDLKGLBNZm2394879jLKJFMMNXKSJi4839902904jdsjlLKJLKJLJBKPPQIWOOIEMMJjmxznm4nmmnzx80zx'
         self.validpassword_specialcharacters = 'Ab1!@#$%^&*()_+-={}[]|:;"hello"<>?,./`~'
-
+        #Invalid
         self.invalidpassword_noupper = 'abcdsf1233'
         self.invalidpassword_nolower = 'ABCEF123'
         self.invalidpassword_nodigit = 'ABCDefgh'
         self.invalidpassword_tooshort = 'Ab1'
         self.invalidpassword_empty = ''
         self.invalidpassword_notascii = 'ABCd二十八28'
-        self.invalidpassword_notprintable = 'ABCdef \t 123 \n 456'
+        self.invalidpassword_notprintable = 'ABCdef\t123\n456'
         self.invalidpassword_withspaces = 'ABcd 12 34'
 
 
 
     def test_username_isvalid(self):
         """
-        Tests the username_isvalid method on validate.py
-        #New no space also allowed
+        Tests the username_isvalid method on validate.py.
+        Username SHOULD NOT include non-ASCII Characters, spaces, new lines, tabs, nor should it be empty.
         """
+        #Valid test cases
         self.assertTrue(username_isvalid(self.validname),'Username should be valid.')
-        self.assertTrue(self.validname_allspaces, 'Username should be valid even if it contains all spaces')
+        self.assertFalse(self.invalidname_allspaces, 'Username should be invalid: Username cannot contain spaces.')
 
+        #Invalid test cases
         self.assertFalse(username_isvalid(self.invalidname_notascii),'Username should be invalid: Username contains non-ASCII characters.')
         self.assertFalse(username_isvalid(self.invalidname_notprintable),'Username should be invalid: Username contains non-printable characters.')
         self.assertFalse(username_isvalid(self.invalidname_empty), 'Username should be invalid: Username is empty string.')
 
-
+    
     def test_password_isvalid(self):
         """
-        Tests the password_isvalid method on validate.py
+        Tests the password_isvalid method on validate.py.
+        Password SHOULD be 8 characters or longer, and must contain at least 1 uppercase, lowercase, and number.
+        Password CANNOT contain new line, tab, non-ASCII, spaces, and cannot be empty.
         """
+        #Valid test cases
         self.assertTrue(password_isvalid(self.validpassword), 'Password should be valid.')
 
         self.assertTrue(password_isvalid(self.validpassword_verylong), 'Password should be valid even though it is very long.')
         self.assertTrue(password_isvalid(self.validpassword_specialcharacters), 'Password should be valid even though it contains special characters.')
 
+        #Invalid test cases
         self.assertFalse(password_isvalid(self.invalidpassword_noupper), 'Password should be invalid: There are no uppercase characters.')
         self.assertFalse(password_isvalid(self.invalidpassword_nolower), 'Password should be invalid: There are no lowercase characters.')
         self.assertFalse(password_isvalid(self.invalidpassword_nodigit), 'Password should be invalid: There are no ASCII numbers.')
